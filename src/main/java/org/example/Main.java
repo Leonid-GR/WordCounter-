146b6c91157b8549b30413924d237a283b71c0f5
leonid <grabovenko.l08@gmail.com>
2026-09-21 17:12:39 +0300

    Add Main class to demonstrate WordCounter functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,19 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        WordCounter counter = new WordCounter();
+
+        counter.countWords("java python java java python code");
+
+        System.out.println("Java: " + counter.getCount("java"));
+        System.out.println("Самое частое слово: " + counter.getMostFrequent());
+
+        System.out.println("Все слова:");
+        counter.printAll();
+
+        System.out.println("Слова, которые встречаются больше 1 раза:");
+        System.out.println(counter.getWordsAbove(1));
     }
 }
